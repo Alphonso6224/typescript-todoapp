d58a4998ca1244f40215cb08bfe550bcc1ac0ad7
donald.ahossi <donaldalphonso11@gmail.com>
2024-07-18 21:34:44 +0200

    Header créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header class="header">
+    <RouterLink to="/">
+      <h1>Todos</h1>
+    </RouterLink>
+
+    <input
+      class="new-todo"
+      type="text"
+      placeholder="Entrez une tâche"
+      autofocus
+      autocomplete="off"
+    />
+  </Header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
