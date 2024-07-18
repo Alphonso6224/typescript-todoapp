e23e726ee53d789a88acfd465c50f04dae8743ed
donald.ahossi <donaldalphonso11@gmail.com>
2024-07-18 22:33:28 +0200

    TodoMain créé

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+        <li :class="{completed: todo.complete}">
+            <div class="view">
+                <input type="checkbox" class="toggle">
+                <Label>
+                    {{ todo.title }}
+                </Label>
+                <button class="destroy"></button>
+            </div>
+        </li>
+</template>
+
+<script setup lang="ts">
+import type { Todo } from '@/@types'
+defineProps<{
+    todo: Todo
+}>();
+</script>
+
+<style scoped></style>
